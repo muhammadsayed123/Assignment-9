ee61783e1c05c4a81428a029ca0e1f2e0ac5dd2d
muhammadsayed123 <muhammadsaye741@gmail.com>
2026-04-27 14:11:38 +0600

    Setup complete and start the project

---
+++ src/Root/Root.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <div className="navbar">
+
+            </div>
+            <div className="main">
+
+            </div>
+            <div className="footer">
+
+            </div>
+        </div>
+    );
+};
+
+export default Root;
